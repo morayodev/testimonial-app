8a8d482732ce6ec45b3e02ac1056e7e78715fc39
morayodev <moryoadelekan41@gmail.com>
2021-12-22 16:11:11 +0100

    worked on adding testimony

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
 
   return (
     <>
+    <div style={{overflowX:"hiiden"}}>
       <Header />
       <HeaderLink />
       <HeroSection />
@@ -22,6 +23,7 @@ const App = () => {
       <HeroMaganize />
       <Footer />
       <StoryNotification showModal={showModal} />
+      </div>
     </>
   );
 };
