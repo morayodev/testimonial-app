e1095e9180a4d5611ae901df3b8884d2f9f03664
morayodev <moryoadelekan41@gmail.com>
2022-03-06 23:44:21 +0100

    header logo added

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,36 +1,25 @@
 import React from "react";
 import "./Header.css";
-import vasitifavicon  from "../../../assets/vasiti.png";
-
+import vasitifavicon from "../../../assets/vasiti.png";
 
 const Header = () => {
   return (
     <>
-    
-      <div className="row justify-content-evenly align-items-center heading">
-        <div className="col-md-6 col-12">
+      <div className="row justify-content-evenly heading align-items-start">
+        <div className="col-md-6 col-12  flex-start ">
           {/* <h1 style={{fontSize:"0.8rem"}}>Vacrsiti</h1> */}
-          <img src={vasitifavicon} alt="vasitiLogo"/>
+          <img src={vasitifavicon} alt="vasitiLogo" />
         </div>
         <div className="col-md-6 col-12">
           <ul className="d-md-flex d-none align-items-center m-0">
-            <li className="px-3 heading-list">
-            ABOUT US
-            </li>
-            <li className="px-3 heading-list">
-           STORES
-            </li>
-            <li className="px-3 heading-list">
-             CONTACT
-            </li>
-            <li className="px-3 heading-list">
-              LOG IN
-            </li>
-              <button className="heading-signup px-3">Sign up</button>
+            <li className="px-3 heading-list">ABOUT US</li>
+            <li className="px-3 heading-list">STORES</li>
+            <li className="px-3 heading-list">CONTACT</li>
+            <li className="px-3 heading-list">LOG IN</li>
+            <button className="heading-signup px-3">Sign up</button>
           </ul>
         </div>
       </div>
-     
     </>
   );
 };
